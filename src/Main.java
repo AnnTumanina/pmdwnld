3e1a5d1006f512507470d1ec89f18a8be0c2d975
AnnTumanina <98632330+AnnTumanina@users.noreply.github.com>
2022-12-01 05:14:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,17 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final String IN_FILE_TXT = "src\\inFile.txt";
-    private static final String MUSIC_OUTPUT_FILE_TXT = "src\\MusicOutputFile.txt";
-    private static final String PICTURE_OUTPUT_FILE_TXT = "src\\PictureOutputFile.txt";
-    private static final String PATH_TO_MUSIC = "src\\Music\\music";
-    private static final String PATH_TO_PICTURE = "src\\Pictures\\picture";
+    private static final String IN_FILE_TXT = "src\\inFile.txt";//файл с сайтом
+    private static final String MUSIC_OUTPUT_FILE_TXT = "src\\MusicOutputFile.txt";//файл с ссылками на скачивание музыки
+    private static final String PICTURE_OUTPUT_FILE_TXT = "src\\PictureOutputFile.txt";//файл с ссылками на скачивание картинки
+    private static final String PATH_TO_MUSIC = "src\\Music\\music";//файл с музыкой
+    private static final String PATH_TO_PICTURE = "src\\Pictures\\picture";//файл с картинкой
 
     public static void main(String[] args) {
         String Url;
-
+//тут поиск ссылок на скачивание музыки
         try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(MUSIC_OUTPUT_FILE_TXT))) {
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(MUSIC_OUTPUT_FILE_TXT))) {//потоки чтения записи ссылок
             while ((Url = inFile.readLine()) != null) {
                 URL url = new URL(Url);
 
@@ -29,23 +29,24 @@ public class Main {
                 try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
                     result = bufferedReader.lines().collect(Collectors.joining("\n"));
                 }
-                Pattern email_pattern = Pattern.compile("href=\"\\/\\/mp3uks.ru\\/mp3\\/files\\/(.+-mp3).mp3");
+                Pattern email_pattern = Pattern.compile("href=\"\\/\\/mp3uks.ru\\/mp3\\/files\\/(.+-mp3).mp3");//регулярное выражение для поиска ссылок
                 Matcher matcher = email_pattern.matcher(result);
                 int i = 0;
                 while (matcher.find() && i < 5) {
-                    outFile.write(matcher.group().replaceAll("href=\"", "") + "\n");
+                    outFile.write(matcher.group().replaceAll("href=\"", "") + "\n");//запись ссылок
                     i++;
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (BufferedReader musicFile = new BufferedReader(new FileReader(MUSIC_OUTPUT_FILE_TXT))) {
+//тут обращение к методу скачивания музыки с передачей парамеров
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(MUSIC_OUTPUT_FILE_TXT))) {//открытие потока чтения файла с ссылками на скачивание музыки
             String music;
             int count = 0;
             try {
                 while ((music = musicFile.readLine()) != null) {
-                    downloadUsingNIO("https:" + music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");
+                    downloadUsingNIO("https:" + music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");//обращение к методу скачивания
                     count++;
                 }
             } catch (IOException e) {
@@ -54,8 +55,9 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//тут поиск ссылок на скачивание картинок
         try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(PICTURE_OUTPUT_FILE_TXT))) {
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(PICTURE_OUTPUT_FILE_TXT))) {//потоки чтения записи ссылок
             while ((Url = inFile.readLine()) != null) {
                 URL url = new URL(Url);
 
@@ -63,10 +65,10 @@ public class Main {
                 try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
                     result = bufferedReader.lines().collect(Collectors.joining("\n"));
                 }
-                Pattern email_pattern = Pattern.compile("//img2.akspic.ru/previews/(.+?).jpg");
+                Pattern email_pattern = Pattern.compile("//img2.akspic.ru/previews/(.+?).jpg");//регулярное выражение для поиска картинок
                 Matcher matcher = email_pattern.matcher(result);
                 int i = 0;
-                while (matcher.find() && i < 5) {
+                while (matcher.find() && i < 5) {//счётчик что их будет 5
                     outFile.write(matcher.group() + "\n");
                     i++;
                 }
@@ -74,12 +76,13 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//тут обращение к методу скачивания картинок с передачей парамеров
         try (BufferedReader musicFile = new BufferedReader(new FileReader(PICTURE_OUTPUT_FILE_TXT))) {
             String picture;
             int count = 0;
             try {
                 while ((picture = musicFile.readLine()) != null) {
-                    downloadUsingNIO("https:" + picture, PATH_TO_PICTURE + String.valueOf(count) + ".jpg");
+                    downloadUsingNIO("https:" + picture, PATH_TO_PICTURE + String.valueOf(count) + ".jpg");//открытие потока чтения файла с ссылками на скачивание картинок
                     count++;
                 }
             } catch (IOException e) {
@@ -89,7 +92,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        try (FileInputStream inputStream = new FileInputStream("src\\Music\\music0.mp3")) {
+        try (FileInputStream inputStream = new FileInputStream("src\\Music\\music0.mp3")) {//прослушивание музыки
             try {
                 Player player = new Player(inputStream);
                 player.play();
@@ -100,11 +103,11 @@ public class Main {
             e.printStackTrace();
         }
     }
-
+//само скачивание с сайта
     private static void downloadUsingNIO(String strUrl, String file) throws IOException {
         URL url = new URL(strUrl);
         ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
-        FileOutputStream stream = new FileOutputStream(file);
+        FileOutputStream stream = new FileOutputStream(file);//запись в папку которая была передана
         stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
         stream.close();
         byteChannel.close();
